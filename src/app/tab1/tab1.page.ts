c057fa405219c5fad8f1d8c86c97ef214c467bdb
Kalex123 <K.a.danvenir@gmail.com>
2020-05-09 00:38:58 -0500

    avance en tab1,cambio en iconos e idioma,nuevo servicio porducto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,26 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import {CartPService} from '../servicios/cart-p.service' 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
+
 export class Tab1Page implements OnInit{
 
+  
+sliderConfig={
+
+  spaceBetween:2,
+  centeredSlides:true,
+  slidesPreview:1.6
+}
+
+
+
+
  
-  // aki se tra jajaja que pendejada xd ya hago 
+  
   cart=[];
 itens=[];
 
-constructor( private cartPService:CartPService){
+constructor( private cartPService:CartPService, private router:Router)
+{
 }
 
 ngOnInit(){
   this.cart=this.cartPService.getCart();
   this.itens=this.cartPService.getProduct();     
-  console.log(this.itens)
+  
   }
+addToCartProduct(product){
+this.cartPService.addProduct(product); 
 
+
+}
+openCart(){
+
+  this.router.navigate(['cartP'])
+}
 
 }
