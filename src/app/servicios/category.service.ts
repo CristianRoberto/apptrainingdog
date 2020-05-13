a4178c1e7b1add23c798f8447a02605f3d0722b2
cristian roberto gilces panta <cristianroberto.16@hotmail.com>
2020-05-13 12:01:21 -0500

    colocacion de nombres y color a categoria

--- src/app/servicios/category.service.ts
+++ src/app/servicios/category.service.ts
@@ -8,7 +8,7 @@ export class CategoryService {
 
   private datas=[
 
-    {id:1,name:'PESCADOS Y MARICOS ', quantity:'20' ,img :"https://mercatperegarau.es/wp-content/uploads/2016/12/315_Francisco-Riera-Llorens-Maria-del-Carmen-del-%C3%81lamo-Acevedo-Peix-Peixateria-Vicenta-Pto-86-87-88_078.jpg"},
+    {id:1,name:'PESCADOS Y MARICOS ' ,img :"https://mercatperegarau.es/wp-content/uploads/2016/12/315_Francisco-Riera-Llorens-Maria-del-Carmen-del-%C3%81lamo-Acevedo-Peix-Peixateria-Vicenta-Pto-86-87-88_078.jpg"},
     {id:1,name:'RESTAURANTE Y COMIDA RAPIDA ', quantity:'20' ,img :"https://www.innaturale.com/cdn-cgi/image/www.innaturale.com/es/wp-content/webp-express/webp-images/doc-root/es/wp-content/uploads/2018/11/restaurantes-de-comida-rapida-menus.jpg.webp"},
     {id:1,name:'TIENDA DE VIVERES ', quantity:'20' ,img :"https://i0.wp.com/exitosanoticias.pe/v1/wp-content/uploads/2020/03/supermercados.png"},
     {id:1,name:'LICORERIAS ', quantity:'20' ,img :"https://www.ultimasnoticias.ec/files/article_main/uploads/2018/04/25/5ae0e9d7df34b.jpeg"},
