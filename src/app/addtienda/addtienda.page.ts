924465e5fb6e6abb33298d7788bc77f837eb07d4
BrunoJavier1925 <javierbruno1925@gmail.com>
2020-05-16 21:04:51 -0500

    commit aggtienda y servicio

--- src/app/addtienda/addtienda.page.ts
+++ src/app/addtienda/addtienda.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TiendaService } from '../servicios/tienda.service';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ModalProductPage } from '../modal/modal-product/modal-product.page';
 
 @Component({
   selector: 'app-addtienda',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addtienda.page.scss'],
 })
 export class AddtiendaPage implements OnInit {
+  constructor(private TiendaService:TiendaService, private router:Router, private modalctr:ModalController ) {
 
-  constructor() { }
+  }
+  
+  tienda=[]
 
-  ngOnInit() {
+  ngOnInit(){
+    //obtiene los productos del servicio ProductoService  y los guarda en un arreglo
+ this.tienda= this.TiendaService.getTienda(); 
+
   }
 
+  
+
+  
 }
